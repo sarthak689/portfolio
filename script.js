1841b6f5449d49e2cf0e8adec58632c90b9778c6
sarthak689 <84018671+sarthak689@users.noreply.github.com>
2021-06-25 22:57:08 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
     
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Learner", "desginer"],
+        strings: ["Learner", "Desginer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
